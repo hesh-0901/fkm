06dc84473be7d1cbdfe38e879c352788416ef112
Hesh <henochshungu@gmail.com>
2026-02-08 15:57:08 +0200

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,10 +1,8 @@
-// Load sidebar HTML
 fetch("/partials/sidebar.html")
   .then(res => res.text())
   .then(html => {
     document.getElementById("sidebar-container").innerHTML = html;
 
-    // Sidebar toggle
     const sidebar = document.getElementById("sidebar");
     const openBtn = document.getElementById("openSidebar");
     const closeBtn = document.getElementById("closeSidebar");
