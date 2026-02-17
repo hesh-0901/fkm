719b93cf6d7b199f042d5562e0f63e2ecaedca39
Hesh <henochshungu@gmail.com>
2026-02-17 04:45:16 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,43 @@
-const CACHE_NAME = "fkm-erp-v1";
+const VERSION = "v2.0.0";
+const CACHE_NAME = `fkm-erp-${VERSION}`;
 
-const urlsToCache = [
+const STATIC_ASSETS = [
   "/fkm/",
   "/fkm/index.html",
-  "/fkm/login.html"
+  "/fkm/login.html",
+  "/fkm/manifest.json"
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => cache.addAll(STATIC_ASSETS))
   );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME)
+          .then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
